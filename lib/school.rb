bbceacffb7a1290e6ac47c7c9bad5378e0810acd
Ryan Erricson <ryanerricson@gmail.com>
2021-02-09 12:55:42 -0500

    commit tree 963bd79d13bbdca94e7837b1c49d6203157525e5

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
 # code here!
+
+class School
+
+    def initialize(roster)
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if roster[grade]
+            roster[grade] << student
+        else
+            roster[grade] = [student]
+        end
+
+    end
+
+    # def roster=(roster)
+    #     @roster = roster
+    # end
+
+    def roster
+        @roster
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        roster.each do |grade, name|
+            name.sort!
+            roster.sort
+        end
+    end
+
+end
